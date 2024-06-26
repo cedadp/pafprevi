ffe120f2a626304a8337ebed332799973f93afde
cedadp <137264521+cedadp@users.noreply.github.com>
2024-06-26 14:24:03 +0200

    Update 3_Verif_Seuil PAF.py

--- pages/3_Verif_Seuil PAF.py
+++ pages/3_Verif_Seuil PAF.py
@@ -6,7 +6,7 @@ from itertools import product
 import locale
 from datetime import datetime, timedelta
 
-st.set_page_config(page_title="Verif Seuil", page_icon="📏", layout="centered", initial_sidebar_state="auto", menu_items=None)
+st.set_page_config(page_title="Verif Seuil", page_icon="📊", layout="centered", initial_sidebar_state="auto", menu_items=None)
 
 hide_streamlit_style = """
             <style>
@@ -17,7 +17,7 @@ hide_streamlit_style = """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
 
-st.title('📏 PAF - Charge horaire / seuil de saturation')
+st.title('📊 PAF - Charge horaire / seuil de saturation')
     
 uploaded_file = st.file_uploader("Selectionner un fichier", type=["xls", "xlsx"])
     
