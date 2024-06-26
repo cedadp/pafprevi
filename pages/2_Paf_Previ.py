f1178cb27a4f9c797896750c7bc724eec98ba1f4
cedadp <137264521+cedadp@users.noreply.github.com>
2024-06-26 14:27:07 +0200

    Update 2_Paf_Previ.py

--- pages/2_Paf_Previ.py
+++ pages/2_Paf_Previ.py
@@ -8,7 +8,7 @@ import openpyxl
 import numpy
 from datetime import datetime, timedelta
   
-st.set_page_config(page_title="Paf Previ", page_icon="🛂", layout="centered", initial_sidebar_state="auto", menu_items=None)
+st.set_page_config(page_title="PAF Previ", page_icon="🛂", layout="centered", initial_sidebar_state="auto", menu_items=None)
 hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
@@ -16,7 +16,7 @@ hide_streamlit_style = """
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
-st.title("🛂 Paf Previ")
+st.title("🛂 PAF Previ")
 st.subheader("Programme complet :")
 
 
