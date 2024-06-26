1c61bebfebd49e094c09fd80d32f43517e64920d
cedadp <137264521+cedadp@users.noreply.github.com>
2024-06-26 13:13:55 +0200

    Add files via upload

--- pages/3_Verif_Seuil PAF.py
+++ pages/3_Verif_Seuil PAF.py
@@ -57,94 +57,93 @@ if uploaded_file is not None:
         
         #fin_semaine_deux = deb_semaine_deux+ pd.Timedelta(days=6)
         
-        if st.button('Créer Export PAF'):    
-
+        
         df['jour']= pd.to_datetime(df['jour'])
         df = df[(df['jour'] >= start_date)&(df['jour']<= end_date)]
         
+        
+        if st.button('Créer Export PAF'):    
+
+             
+            df = df.sort_values(by=['site', 'jour', 'heure'])
+    
+    #transforme le dataframe dans un format "large"        
+            
+            df_pivot = df.pivot (index=['site', 'jour'], columns='heure', values='charge').reset_index() 
+            
+            
+    # calcul du cumul de charge horaire glissant à partir de données 10 min
+            
+            def calculate_sums(row): 
+                sums = {}
+                times = df_pivot.columns[2:]
+                for i in range(len(times)-5):
+                    sum_value=sum(row[times[j]] for j in range (i,i+6))
+                    sums[times[i+5]] = sum_value
+                return pd.Series(sums)
+            
+            new_df = df_pivot.apply (calculate_sums, axis=1)
+                
+            new_df.insert(0, 'site', df_pivot['site'])
+            new_df.insert(1, 'jour', df_pivot['jour'])        
+        
+        # retourne le dataframe dans un format "long"
+        
+            df_depivote = pd.melt(new_df, id_vars=['site', 'jour'], var_name='heure', value_name='charge')
+    
+             
+                                      
+    #new_df.to_excel("C:/Users/demanet/Downloads/test_cumul.xlsx", index= False)        
+            
       
-       
-        
-       
-        df = df.sort_values(by=['site', 'jour', 'heure'])
-
-#transforme le dataframe dans un format "large"        
-        
-        df_pivot = df.pivot (index=['site', 'jour'], columns='heure', values='charge').reset_index() 
-        
-        
-# calcul du cumul de charge horaire glissant à partir de données 10 min
-        
-        def calculate_sums(row): 
-            sums = {}
-            times = df_pivot.columns[2:]
-            for i in range(len(times)-5):
-                sum_value=sum(row[times[j]] for j in range (i,i+6))
-                sums[times[i+5]] = sum_value
-            return pd.Series(sums)
-        
-        new_df = df_pivot.apply (calculate_sums, axis=1)
             
-        new_df.insert(0, 'site', df_pivot['site'])
-        new_df.insert(1, 'jour', df_pivot['jour'])        
-    
-    # retourne le dataframe dans un format "long"
-    
-        df_depivote = pd.melt(new_df, id_vars=['site', 'jour'], var_name='heure', value_name='charge')
-
-         
-                                  
-#new_df.to_excel("C:/Users/demanet/Downloads/test_cumul.xlsx", index= False)        
-        
-  
-        
-#créer une liste dynamique des sites (revient à faire unique(), mais permet ici de supprmier les sites fermées)
-        df_config =df.groupby(['site'], as_index=False)['charge'].sum()
-        df_config =df_config[df_config['charge'] > 0]
-        df_config = df_config.drop(columns=['charge'])
-        
-                  
-        custom_dict = {'K CTRCNT' : 0,'K CTR' : 1,'K CNT' : 2 , 'L CTR' : 3, 'L CNT' : 4, 'M CTR' : 5, 'Galerie EF' : 6, 'C2F' : 7, 'C2G' : 8, 'Liaison AC' : 9,'Liaison BD' : 10,
-        'T3': 11, 'Terminal 1' : 12, 'Terminal 1_5' : 13, 'Terminal 1_6' : 14}
-        
-        df_config = df_config.sort_values(by=['site'], key=lambda x: x.map(custom_dict)).reset_index(drop=True)
-    
-        
-        
-        
-        
-        
-        sites = df_config['site'].unique() # permet de créer un tableau simple à partir du dataframe df_complet (à revoir)
-        
-        
-    # seuils de saturation    
-        
-        
-        def seuil(site):
-            seuils = {'K CTRCNT' : 0,'K CTR' : 1600,'K CNT' : 300 , 'L CTR' : 2100, 'L CNT' :  1440, 'M CTR' : 1820, 'Galerie EF' : 1820, 'C2F' : 2180, 'C2G' : 910, 'Liaison AC' : 1960,'Liaison BD' : 2500,
-            'T3': 1260, 'Terminal 1' : 2280, 'Terminal 1_5' : 375, 'Terminal 1_6' : 500}
-        
-        
-            return seuils.get(site,0)
-        
-    
-    # graphique   
-    
-        for site in sites: 
+    #créer une liste dynamique des sites (revient à faire unique(), mais permet ici de supprmier les sites fermées)
+            df_config =df.groupby(['site'], as_index=False)['charge'].sum()
+            df_config =df_config[df_config['charge'] > 0]
+            df_config = df_config.drop(columns=['charge'])
             
-            st.subheader(f" {site}")
+                      
+            custom_dict = {'K CTRCNT' : 0,'K CTR' : 1,'K CNT' : 2 , 'L CTR' : 3, 'L CNT' : 4, 'M CTR' : 5, 'Galerie EF' : 6, 'C2F' : 7, 'C2G' : 8, 'Liaison AC' : 9,'Liaison BD' : 10,
+            'T3': 11, 'Terminal 1' : 12, 'Terminal 1_5' : 13, 'Terminal 1_6' : 14}
             
-            st.write("seuil max: ", seuil(site))
-            df_site = df_depivote.loc[df_depivote['site']==site]
-            locale.setlocale(locale.LC_ALL, 'fr_FR')
-            df_site['Date']= df_site['jour'].dt.strftime('%A %d %b')
-           
-            fig = px.line(df_site, x= 'heure', y= 'charge',  color = 'Date',
-                            labels={'jour', 'date'})
-            
-            ligne_seuil = seuil(site)
-            fig.add_hline(y=ligne_seuil, line_dash='dash', line_color="red")
+            df_config = df_config.sort_values(by=['site'], key=lambda x: x.map(custom_dict)).reset_index(drop=True)
+        
             
             
             
-            st.plotly_chart(fig)
+            
+            
+            sites = df_config['site'].unique() # permet de créer un tableau simple à partir du dataframe df_complet (à revoir)
+            
+            
+        # seuils de saturation    
+            
+            
+            def seuil(site):
+                seuils = {'K CTRCNT' : 0,'K CTR' : 1600,'K CNT' : 300 , 'L CTR' : 2100, 'L CNT' :  1440, 'M CTR' : 1820, 'Galerie EF' : 1820, 'C2F' : 2180, 'C2G' : 910, 'Liaison AC' : 1960,'Liaison BD' : 2500,
+                'T3': 1260, 'Terminal 1' : 2280, 'Terminal 1_5' : 375, 'Terminal 1_6' : 500}
+            
+            
+                return seuils.get(site,0)
+            
+        
+        # graphique   
+        
+            for site in sites: 
+                
+                st.subheader(f" {site}")
+                
+                st.write("seuil max: ", seuil(site))
+                df_site = df_depivote.loc[df_depivote['site']==site]
+                locale.setlocale(locale.LC_ALL, 'fr_FR')
+                df_site['Date']= df_site['jour'].dt.strftime('%A %d %b')
+               
+                fig = px.line(df_site, x= 'heure', y= 'charge',  color = 'Date',
+                                labels={'jour', 'date'})
+                
+                ligne_seuil = seuil(site)
+                fig.add_hline(y=ligne_seuil, line_dash='dash', line_color="red")
+                
+                
+                
+                st.plotly_chart(fig)
