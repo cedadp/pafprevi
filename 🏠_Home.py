4591a817001709d68245b935c6287ee58b98060f
cedadp <137264521+cedadp@users.noreply.github.com>
2024-07-01 14:26:56 +0200

    Update 🏠_Home.py

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -1,14 +1,14 @@
 import pandas as pd
 import streamlit as st
 
-st.set_page_config(page_title="PAF Prévi", page_icon="🛂", layout="centered", initial_sidebar_state="auto")
+st.set_page_config(page_title="PAF Prévis", page_icon="🛂", layout="centered", initial_sidebar_state="auto")
 
 
 st.title('Prévision flux DPAF') 
 
 
 st.markdown("Onglet ** 📦 Concat** : Un outil de concaténation des programmes AF Skyteam et des programmes SariaP.")
-st.markdown("Onglet ** 🛂 Paf Prévi** : Un outil de prévisions des flux aux différents sites DPAF de l'aéroport CDG.")
+st.markdown("Onglet ** 🛂 Paf Prévis** : Un outil de prévisions des flux aux différents sites DPAF de l'aéroport CDG.")
 st.markdown("Onglet ** 📊 Vérif Seuil** : Un outil pour tracer le débit horaire des flux DPAF.")
 
 st.sidebar.info("Version : 1.0")
